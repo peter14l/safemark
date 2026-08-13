7a6a3cd076bb3bea05840727a591789dbb216a0e
peter14l <peter14l@users.noreply.github.com>
2026-08-13 21:44:33 +0530

    Bump version to 1.6.2: Ensure WebRTC media tracks and peer connections are fully released on CallScreen unmount to save battery

--- app/(app)/call.tsx
+++ app/(app)/call.tsx
@@ -158,6 +158,11 @@ export default function CallScreen() {
       stopAudioSession();
       stopRingtone();
       stopRingback();
+      if (user) {
+        endCall(user.id).catch((err) =>
+          console.error("Error releasing call resources on unmount:", err)
+        );
+      }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
